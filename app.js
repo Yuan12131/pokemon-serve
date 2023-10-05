a0a6e0dd1a1faeb99b37c2d550b96980aaccc6d6
leeyuan <dbdks32123@gmail.com>
2023-10-05 20:04:26 +0900

    setTimeOut 적용중

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const http = require('http');
 const fs = require('fs');
+const { setTimeout } = require('timers/promises');
 
 const server = http.createServer(function(request, response){
   response.writeHead(200, {"Content-Type" : "text/html"});
 
   if(request.url === "/"){
-    let doc = '<!DOCTYPE html> <head> <meta charset="UTF-8"> </head> <body> <a href = "http://localhost:8019/sub"> html문서 띄우기 </a> </body> </html>'
+    
+    setTimeout(2000, function(){
+      location.href="http://localhost:8019/sub";
+    })
+
+    let doc = '<!DOCTYPE html> <head> <meta charset="UTF-8"> </head> <body> <a href = "http://localhost:8019/sub"> 서브로이동하기 </a> </body> </html>'
+
     response.end(doc);
   }
+  
   if(request.url === "/sub"){
-    fs.readFile("./vending_2.html", function(err, data){
+    fs.readFile("./class_setTimeOut.html", function(err, data){
       if (err){
         console.error("파일을 읽지 못했습니다.");
       } else {
